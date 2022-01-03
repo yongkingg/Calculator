39285b3cc43ff8656b2af91356f85baccde3518d
unknown <dydwns8064@naver.com>
2022-01-03 20:48:49 +0900

    Calculator Ui set

--- Test.py
+++ Test.py
@@ -13,20 +13,21 @@ class Main:
         self.ui = Ui.Ui()
         self.connectEvent()
         self.inputNumber = ""
+        self.saveNumber = []
+
     def connectEvent(self):
         for index in range(0,len(self.ui.numberBtn)):
             self.ui.numberBtn[index].mousePressEvent = lambda event, num = index: self.numberEvent(event, num)
             self.ui.numberBtn[index].enterEvent = lambda event, num = index: self.EnterEvent(event, num, 1)
             self.ui.numberBtn[index].leaveEvent = lambda event, num = index: self.LeaveEvent(event, num, 1)
-        for index in range(0,len(self.ui.calculate)):
-            self.ui.calculate[index].mousePressEvent = lambda event, num = index: self.calculate(event, num)
-            self.ui.calculate[index].enterEvent = lambda event, num = index: self.EnterEvent(event, num, 2)
-            self.ui.calculate[index].leaveEvent = lambda event, num = index: self.LeaveEvent(event, num, 2)
+        for index in range(0,len(self.ui.operatorBtn)):
+            self.ui.operatorBtn[index].mousePressEvent = lambda event, num = index: self.calculate(event, num)
+            self.ui.operatorBtn[index].enterEvent = lambda event, num = index: self.EnterEvent(event, num, 2)
+            self.ui.operatorBtn[index].leaveEvent = lambda event, num = index: self.LeaveEvent(event, num, 2)
         for index in range(0,len(self.ui.toolBtn)):
             self.ui.toolBtn[index].mousePressEvent = lambda event, num = index: self.calculate(event, num)
             self.ui.toolBtn[index].enterEvent = lambda event, num = index: self.EnterEvent(event, num, 3)
             self.ui.toolBtn[index].leaveEvent = lambda event, num = index: self.LeaveEvent(event, num, 3)
-
 
     def EnterEvent(self,event,index,judge):
         if judge == 1:
@@ -35,16 +36,15 @@ class Main:
                 "background-color : grey;"
                 )
         elif judge == 2:
-            self.ui.calculate[index].setStyleSheet(
-                "border : 1px solid black;"
+            self.ui.operatorBtn[index].setStyleSheet(
+                "border : 2px solid black;"
                 "background-color : grey;"
                 )
         elif judge == 3:
             self.ui.toolBtn[index].setStyleSheet(
-                "border : 1px solid black;"
+                "border : 2px solid black;"
                 "background-color : grey;"
                 )
-
     def LeaveEvent(self,event,index,judge):
         if judge == 1:
             self.ui.numberBtn[index].setStyleSheet(
@@ -52,31 +52,34 @@ class Main:
                 "background-color : lightgrey;"
                 )
         elif judge == 2:
-            self.ui.calculate[index].setStyleSheet(
-                "border : 1px solid black;"
+            self.ui.operatorBtn[index].setStyleSheet(
+                "border : 2px solid black;"
                 "background-color : lightgrey;"
                 )
         elif judge == 3:
             self.ui.toolBtn[index].setStyleSheet(
-                "border : 1px solid black;"
+                "border : 2px solid black;"
                 "background-color : lightgrey;"
                 )
     def numberEvent(self,event,index):
         initNumber = []
-        initNumber.append(index)
+        initNumber.append(index+1)
         for index in range(0,len(initNumber)):
             self.inputNumber = self.inputNumber + str(initNumber[index])
             self.ui.resultBox.setText(self.inputNumber)
-    def plus(self):
-        pass
+        print(initNumber)
+    def plus(self,event,index,):
+        print(index)
     def minus(self):
         pass
     def multiply(self):
         pass
     def division(self):
         pass
-    def calculate(self):
-        pass
+    def calculate(self,event,index):
+        self.saveNumber.append(self.inputNumber)
+        self.inputNumber = ""
+    
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
--- Ui.py
+++ Ui.py
@@ -4,64 +4,67 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui:
     def __init__(self):
         self.mainWindow = QtWidgets.QMainWindow()
-        self.mainWindow.resize(400,500)
+        self.mainWindow.resize(500,810)
+        self.mainWindow.setMaximumSize(500,810)
+        self.mainWindow.setMinimumSize(500,810)
+
         self.mainWindow.setWindowTitle("계산기")
         self.centralWidget = QtWidgets.QWidget(self.mainWindow)
-        self.centralWidget.setGeometry(0,0,400,500)
-
+        self.centralWidget.setGeometry(0,0,800,1000)
+        
         self.resultBox = QtWidgets.QLabel(self.mainWindow)
-        self.resultBox.setGeometry(40,20,320,70)
+        self.resultBox.setGeometry(40,20,430,110)
         self.resultBox.setStyleSheet(
-            "border : 1px solid black;"
+            "border : 2px solid black;"
             )
-        self.resultBox.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+        self.resultBox.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignVCenter)
         self.resultBox.setFont(QtGui.QFont("궁서",30))
 
-        self.calculate = []
-        self.sign = ["+","-","X","/"]
-        for index in range(0,4):
+        self.operatorBtn = []
+        self.operatorList = ["=","+","-","×","÷","Del"]
+        for index in range(0,len(self.operatorList)):
             tmpBtn = QtWidgets.QPushButton(self.centralWidget)
-            if index < 2:
-                xPos = 40 + 50*index
-            else:
-                xPos = 150 + 50*(index-2)
-            tmpBtn.setGeometry(xPos,100,50,50)
+            yPos = 690 - 110*index
+            tmpBtn.setGeometry(370,yPos,100,100)
             tmpBtn.setStyleSheet(
-                "border : 1px solid black;"
-                "background-color : lightgrey"
+                "border : 2px solid black;"
+                "background-color : lightgrey;"
                 )
-            tmpBtn.setText(self.sign[index])
-            self.calculate.append(tmpBtn)
-
-        self.toolBtn = []
-        self.toolList = ["C","="]
-        for index in range(0,2):
-            tmpBtn = QtWidgets.QPushButton(self.centralWidget)
-            xPos = 260 + 50*index
-            tmpBtn.setGeometry(xPos,100,50,50)
-            tmpBtn.setStyleSheet(
-                "border : 1px solid black;"
-                "background-color : lightgrey"
-                )
-            tmpBtn.setText(self.toolList[index])
-            self.toolBtn.append(tmpBtn)
-
+            tmpBtn.setText(self.operatorList[index])
+            self.operatorBtn.append(tmpBtn)
+        
         self.numberBtn = []
-        for index in range(0,9):
+        self.additionList = ["+/-","0","."]
+        for index in range(0,12):
             tmpBtn = QtWidgets.QPushButton(self.centralWidget)
-            if index < 3:
-                yPos = 160
-            elif index < 6:
-                yPos = 270
-            else:
-                yPos = 380
             xPos = 40 + 110*(index%3)
+            yPos = 360 + 110*(index//3)
             tmpBtn.setGeometry(xPos,yPos,100,100)
             tmpBtn.setStyleSheet(
                 "border : 1px solid black;"
                 "background-color : lightgrey;"
                 )
-            tmpBtn.setText(str(index + 1))
+            if index < 9:
+                tmpBtn.setText(str(index + 1))
+            else:
+                tmpBtn.setText(self.additionList[index-9])
             self.numberBtn.append(tmpBtn)
             
+        self.toolBtn = []
+        self.toolList = ["%","CE","C","1/x","x^2","√"]
+        for index in range(0,len(self.toolList)):
+            tmpBtn = QtWidgets.QPushButton(self.centralWidget)
+            xPos = 40 + 110*(index%3)
+            yPos = 140 + 110*(index//3)
+            tmpBtn.setGeometry(xPos,yPos,100,100)
+            tmpBtn.setStyleSheet(
+                "border : 2px solid black;"
+                "background-color : lightgrey;"
+                )
+            tmpBtn.setText(self.toolList[index])
+            self.toolBtn.append(tmpBtn)
+
+
+
+
         self.mainWindow.show()
